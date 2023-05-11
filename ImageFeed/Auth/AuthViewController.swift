0b67b7c92d440eb7581fd363034ab916bd3bd091
Roman Bystrov <regspeedy@gmail.com>
2023-05-12 00:00:45 +0300

    10th

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Роман on 11.05.2023.
+//
+
+import Foundation
+
+class AuthViewController {
+    
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Роман on 11.05.2023.
+//
+
+import Foundation
+
+class WebViewViewController {
+    
+}
