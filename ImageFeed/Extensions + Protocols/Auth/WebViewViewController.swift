6733a54e2842db338f75554930508370bc4d6f61
Roman Bystrov <regspeedy@gmail.com>
2023-08-24 16:23:17 +0300

    first comm

--- ImageFeed/Extensions + Protocols/Auth/WebViewViewController.swift
+++ ImageFeed/Extensions + Protocols/Auth/WebViewViewController.swift
@@ -1,39 +1,48 @@
 import UIKit
 import WebKit
 
-final class WebViewViewController: UIViewController {
+final class WebViewViewController: UIViewController & WebViewViewControllerProtocol {
     
     weak var delegate: WebViewViewControllerDelegate?
     private var estimatedProgressObservation: NSKeyValueObservation?
     
     @IBOutlet private var progressView: UIProgressView!
     @IBOutlet private var webView: WKWebView!
+    var presenter: WebViewPresenterProtocol?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        updateProgress()
     }
     
     @IBAction private func didTapBackButton(_ sender: Any?) {
         delegate?.webViewViewControllerDidCancel(self)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        estimatedProgressObservation = webView.observe(\.estimatedProgress, options: [], changeHandler: {[weak self] _, _ in
-            guard let self = self else { return }
-            self.updateProgress()
-        })
-        
-        webView.navigationDelegate = self
-        webViewLoad()
-        updateProgress()
+    func load(request: URLRequest) {
+        webView.load(request)
     }
     
-    private func updateProgress() {
-        progressView.progress = Float(webView.estimatedProgress)
-        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter?.webViewLoad()
+        webView.navigationDelegate = self
+        estimatedProgressObservation = webView.observe(\.estimatedProgress, options: [], changeHandler: {[weak self] _, _ in
+            guard let self = self else { return }
+            self.presenter?.didUpdateProgressValue(self.webView.estimatedProgress)
+        })
+    }
+    
+//    private func updateProgress() {
+//        progressView.progress = Float(webView.estimatedProgress)
+//        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+//    }
+    
+    func setProgressValue(_ newValue: Float) {
+        progressView.progress = newValue
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+        progressView.isHidden = isHidden
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -53,32 +62,11 @@ extension WebViewViewController: WKNavigationDelegate {
             decisionHandler(.allow)
         }
     }
+    
     private func code(from navigationAction: WKNavigationAction) -> String? {
-        if
-            let url = navigationAction.request.url,
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: {$0.name == "code"})
-        {
-            return codeItem.value
-        } else {
-            return nil
+        if let url = navigationAction.request.url {
+            return presenter?.code(from: url)
         }
-    }
-}
-
-private extension WebViewViewController {
-    func webViewLoad() {
-        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)
-        urlComponents?.queryItems = [
-            URLQueryItem(name: "client_id", value: AccessKey),
-            URLQueryItem(name: "redirect_uri", value: RedirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: AccessScope)
-        ]
-        guard let url = urlComponents?.url else { return }
-        let request = URLRequest(url: url)
-        webView.load(request)
+        return nil
     }
 }
---
+++ ImageFeed/Extensions + Protocols/AuthConfiguration.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+let AccessKey = "E1trHJ0Js5C_fT0MNSESA9bd2L0aOL5bfzvAXDyYxLc"
+let SecretKey = "hS-uSzsT_haBYLNHymKpKlQuaoZsjZEDhZ14cIi_n8c"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let BaseURL = "https://unsplash.com"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let baseURL: String
+    let defaultBaseURL: URL
+    let unsplashAuthorizeURLString: String
+    
+    static var standart: AuthConfiguration {
+        return AuthConfiguration(accessKey: AccessKey, secretKey: SecretKey, redirectURI: RedirectURI, accessScope: AccessKey, baseURL: BaseURL, defaultBaseURL: DefaultBaseURL, unsplashAuthorizeURLString: UnsplashAuthorizeURLString)
+    }
+
+    
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, baseURL: String, defaultBaseURL: URL, unsplashAuthorizeURLString: String) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.baseURL = baseURL
+        self.defaultBaseURL = defaultBaseURL
+        self.unsplashAuthorizeURLString = unsplashAuthorizeURLString
+    }
+}
